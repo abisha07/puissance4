6e99dcb6ef7b10267139f2ced7a3ebb0c9439171
abisha07 <jeyavelabisha9@gmail.com>
2022-01-25 12:22:02 +0100

    ojpjo

--- projetIA/src/projetIA/up/mi/jr/TestJa.java
+++ projetIA/src/projetIA/up/mi/jr/TestJa.java
@@ -1,10 +1,11 @@
 package projetIA.up.mi.jr;
 
-public class TestJava {
+public class TestJa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi");
+
 	}
 
 }
