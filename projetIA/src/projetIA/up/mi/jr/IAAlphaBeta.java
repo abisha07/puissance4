f216ac3375800a8daf53a97855c01ff46cdd89de
prisca1130 <niainaramanantoanina@yahoo.fr>
2022-03-26 21:26:11 +0100

    AlphaBeta

--- projetIA/src/projetIA/up/mi/jr/IAAlphaBeta.java
+++ projetIA/src/projetIA/up/mi/jr/IAAlphaBeta.java
@@ -1,6 +1,7 @@
 package projetIA.up.mi.jr;
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class IAAlphaBeta extends Joueur{
 	//private int niveau = 2;
@@ -15,12 +16,50 @@ public class IAAlphaBeta extends Joueur{
 	}
 	
 
-
-
 	@Override
 	public int trouverPlacement(Plateau plateau) {
-		// TODO Auto-generated method stub
-		return 0;
+			ArrayList<Integer> colonnesAJouer = new ArrayList<Integer>();
+		
+		// On initialise les résultat avec la première colonne jouable pour éviter
+		// que l'IA ne selectionne une colonne non jouable par défaut
+		for(int i = 1; i <= 6; i++){
+				if(plateau.grilleJeu[0][i -1] == '.'){
+					colonnesAJouer.add(i);
+					break;
+				}
+			} 
+
+		double valeurDeJeu = heuristique.getMinScore();
+		for(int i=1; i <= 6; i++){
+			try {
+				if(plateau.grilleJeu[0][i -1] == '.'){
+					//DEEP COPY
+					char [][] copieGrille = new char[6][7];
+					for (int t=0;t<copieGrille.length;t++) {
+						copieGrille[t] = Arrays.copyOf(plateau.grilleJeu[t], plateau.grilleJeu[t].length);
+					} 
+					Plateau copieJeu = new Plateau(copieGrille);
+					copieJeu.placerJeton(i, this);
+					
+					double valeurDeJeuCourante = alphabeta(copieJeu, this, profondeur);
+					if (valeurDeJeuCourante == valeurDeJeu){
+						colonnesAJouer.add(i);
+					}else if(valeurDeJeuCourante > valeurDeJeu){
+						colonnesAJouer.clear();
+						valeurDeJeu = valeurDeJeuCourante;
+						colonnesAJouer.add(i);
+					}
+				}
+			} catch (PuissanceException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		int numeroDeColonneAJouer = (int) (Math.random() * colonnesAJouer.size());
+		
+		
+		
+		return colonnesAJouer.get(numeroDeColonneAJouer);
 	}
 	
 	
@@ -32,14 +71,18 @@ public class IAAlphaBeta extends Joueur{
 	
 	private double min(Plateau plateau, Joueur joueur,  int profondeur, double alpha, double beta){
 		if(profondeur != 0){
-			double valeurDeJeu = Heuristique.MAX_NOTE;
-			for(int i=1; i <= Grille.LARGEUR_GRILLE ; i++){
+			double valeurDeJeu = heuristique.getMaxScore();
+			for(int i=1; i <= 6 ; i++){
 				try {
-					if(!grille.estColonnePleine(i)){
-						Grille copieDeLaGrille = grille.copie();
-						Jeton jeton = new Jeton(Partie.getPartie().joueurSuivant(joueur).getCouleur());
-						copieDeLaGrille.insereJeton(i, jeton);
-						valeurDeJeu = Math.min(valeurDeJeu, this.max(copieDeLaGrille, joueur, profondeur-1, alpha, beta));
+					if(plateau.grilleJeu[0][i -1] == '.'){
+						//DEEP COPY
+						char [][] copieGrille = new char[6][7];
+						for (int t=0;t<copieGrille.length;t++) {
+							copieGrille[t] = Arrays.copyOf(plateau.grilleJeu[t], plateau.grilleJeu[t].length);
+						} 
+						Plateau copieJeu = new Plateau(copieGrille);
+						copieJeu.placerJeton(i, this);
+						valeurDeJeu = Math.min(valeurDeJeu, this.max(copieJeu, joueur, profondeur-1, alpha, beta));
 						
 						if(alpha >= valeurDeJeu){
 							return valeurDeJeu; // Coupure alpha
@@ -48,17 +91,46 @@ public class IAAlphaBeta extends Joueur{
 		               beta = Math.min(beta, valeurDeJeu);
 						
 					}
-				} catch (HorsIndexException e) {
+				} catch (PuissanceException e) {
 					e.printStackTrace();
-				}catch (ColonnePleineException e) {
-					e.printStackTrace();
-				} 
+				}
 			}
 			return valeurDeJeu;
 		}else{
-			return this.heuristique.noteGrille(grille, joueur );
+			return this.heuristique.randomHeuristique();
 		}
 	}
 	
-
+	private double max(Plateau plateau, Joueur joueur, int profondeur, double alpha, double beta){
+		if(profondeur != 0){
+			double valeurDeJeu = heuristique.getMinScore();
+			for(int i=1; i <= 6; i++){
+				try{
+					if(plateau.grilleJeu[0][i -1] == '.'){
+						//DEEP COPY
+						char [][] copieGrille = new char[6][7];
+						for (int t=0;t<copieGrille.length;t++) {
+							copieGrille[t] = Arrays.copyOf(plateau.grilleJeu[t], plateau.grilleJeu[t].length);
+						} 
+						Plateau copieJeu = new Plateau(copieGrille);
+						copieJeu.placerJeton(i, this);
+						valeurDeJeu = Math.max(valeurDeJeu, this.min(copieJeu, joueur, profondeur-1, alpha, beta));
+		
+						if(valeurDeJeu >= beta){
+							return valeurDeJeu; // Coupure beta
+						}
+						alpha = Math.max(alpha	, valeurDeJeu);
+						
+					}
+				} catch (PuissanceException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			return valeurDeJeu;
+		}else{
+			return this.heuristique.randomHeuristique();
+		}
+	
+	}
 }
